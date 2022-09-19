e5a65f49af3a1d9d26c6a0b3470604b5b5a0221b
charles busso <charlesagbusso13@gmail.com>
2022-09-19 18:56:52 -0300

    Ok

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ function carregar() {
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
-    var minutes = date.getMinutes();
+    var minuto = date.getMinutes();
    
-    msg.innerHTML = `Agora são ${hora} horas.`
+    msg.innerHTML = `Agora são ${hora,minuto} horas.`
     if (hora >= 0 && hora < 12) {
         // BOM DIA !!
         document.body.style.background = '#e2cd9f'
