76e9f364d171be73a1ebeb7dd81ca64d76181aa7
charles busso <charlesagbusso13@gmail.com>
2022-09-19 22:19:05 -0300

    Ok

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ function carregar() {
     var msg= window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date()
-    var hora = data.getHours;
-    var minuto = data.getMinutes;
-    Var segundo = data.getSeconds;
+    var hora = data.getHours();
+    var minuto = data.getMinutes();
+    Var segundo = data.getSeconds();
 
    var tempo_total = hora + ":" + minuto ":" + segundo;
     if (hora >= 0 && hora < 6) {
